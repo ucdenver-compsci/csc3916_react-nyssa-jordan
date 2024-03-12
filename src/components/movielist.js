26a9d0ea95819138ab948b05b3046df0014041ac
nyssa-jordan <jordannyssa@gmail.com>
2024-03-11 21:37:20 -0600

    Update movielist.js

--- src/components/movielist.js
+++ src/components/movielist.js
@@ -35,22 +35,23 @@ class MovieList extends Component {
             }
 
             return (
-                <Carousel onSelect={this.handleSelect}>
-                    {movieList.map((movie) =>
-                        <Carousel.Item key={movie._id}>
-                            <div>
-                                <LinkContainer to={'/movie/'+movie._id} onClick={()=>this.handleClick(movie)}>
-                                    <Nav.Link><Image className="image" src={movie.imageUrl} thumbnail /></Nav.Link>
-                                </LinkContainer>
-                            </div>
-                            <Carousel.Caption>
-                                <h3>{movie.title}</h3>
-                                <BsStarFill glyph={'star'} /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                    )}
+                <div></div>
+                // <Carousel onSelect={this.handleSelect}>
+                //     {movieList.map((movie) =>
+                //         <Carousel.Item key={movie._id}>
+                //             <div>
+                //                 <LinkContainer to={'/movie/'+movie._id} onClick={()=>this.handleClick(movie)}>
+                //                     <Nav.Link><Image className="image" src={movie.imageUrl} thumbnail /></Nav.Link>
+                //                 </LinkContainer>
+                //             </div>
+                //             <Carousel.Caption>
+                //                 <h3>{movie.title}</h3>
+                //                 <BsStarFill glyph={'star'} /> {movie.avgRating} &nbsp;&nbsp; {movie.releaseDate}
+                //             </Carousel.Caption>
+                //         </Carousel.Item>
+                //     )}
 
-                </Carousel>
+                // </Carousel>
             )
         }
 
